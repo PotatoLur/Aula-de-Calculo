b841643291a895dd923a14811f745f9d41a75eb0
PotatoLur <guilherme.eguchi@sptech.school>
2025-08-21 18:25:04 -0300

    Corrigindo tarefa script leitura

--- Aula3/ScriptLeitura/script_leitura.py
+++ Aula3/ScriptLeitura/script_leitura.py
@@ -1,4 +1,35 @@
+import psutil
 import pandas as pd
+import time
+import datetime
+
+dados = {
+    "timestamp": [],
+    "cpu": [],
+    "ram": [],
+    "disco": [],
+}
+
+i = 0
+while i < 10:
+    dataTempo = datetime.datetime.now()
+    cpu = psutil.cpu_percent(percpu = False)
+    ram = psutil.virtual_memory()
+    disco = psutil.disk_usage('/')
+
+    dados['timestamp'].append(dataTempo)
+    dados['cpu'].append(cpu)
+    dados['ram'].append(round(float(ram.used)/1000000000, 2))
+    dados['disco'].append((disco.used)/1000000000)
+    i += 1
+    print(dados)
+    time.sleep(10)
+
+df = pd.DataFrame(dados)
+
+df.to_csv("coleta-dos-dados.csv", encoding="utf-8", index=False)
+
+
 
 dados = pd.read_csv("coleta-dos-dados.csv")
 
